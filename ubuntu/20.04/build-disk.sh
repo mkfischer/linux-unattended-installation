0dff193317f1525d8017626035d669b7f41eb58a
Mike Fischer <mkfischer1970@icloud.com>
2021-01-03 10:12:54 -0600

    foo

--- ubuntu/20.04/build-disk.sh
+++ ubuntu/20.04/build-disk.sh
@@ -1,46 +1,21 @@
 #!/bin/bash
-set -e
-
+set -x
 # lookup specific binaries
 : "${BIN_QEMU_IMG:=$(type -P qemu-img)}"
 : "${BIN_KVM:=$(type -P kvm)}"
-
 # get parameters
 RAM_SIZE=${1:-"1024"}
 DISK_SIZE=${2:-"8G"}
 DISK_FORMAT=${3:-"qcow2"}
 SSH_PUBLIC_KEY_FILE=${4:-"$HOME/.ssh/id_rsa.pub"}
 DISK_FILE=${5:-"$(pwd)/ubuntu-20.04-amd64-$RAM_SIZE-$DISK_SIZE.$DISK_FORMAT"}
-
 # create iso
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TMP_ISO_DIR="`mktemp -d`"
 eval "$SCRIPT_DIR/build-iso.sh" "$SSH_PUBLIC_KEY_FILE" "$TMP_ISO_DIR/ubuntu-20.04-netboot-amd64-unattended.iso"
-
-#if [ ${HOSTNAME} == "jenkins" ]
-#then
-#    BIN_QEMU_IMG="/usr/bin/qemu-img"
-#    BIN_KVM="/usr/bin/kvm"
-#else
-#    exit(1)
-#fi
-
-# test
-#if [[ -z ${BIN_QEMU_IMG} ]] || [[ -z ${BIN_QEMU_IMG} ]]
-#then
-#    /usr/bin/printf "\nCan not find qemu-img or kvm\n"
-#    exit(1)
-#fi
-
-# create image and run installer
-#"${BIN_QEMU_IMG}" create "$DISK_FILE" -f "$DISK_FORMAT" "$DISK_SIZE"
-#"${BIN_KVM}" -nographic -cpu host -m "$RAM_SIZE" -cdrom "$TMP_ISO_DIR/ubuntu-20.04-netboot-amd64-unattended.iso" -boot once=d "$DISK_FILE"
-
-/usr/bin/qemu-img create "$DISK_FILE" -f "$DISK_FORMAT" "$DISK_SIZE"
-/usr/bin/kvm -nographic -cpu host -m "$RAM_SIZE" -cdrom "$TMP_ISO_DIR/ubuntu-20.04-netboot-amd64-unattended.iso" -boot once=d "$DISK_FILE"
-
+sh '/usr/bin/qemu-img create "$DISK_FILE" -f "$DISK_FORMAT" "$DISK_SIZE"'
+sh '/usr/bin/kvm -nographic -cpu host -m "$RAM_SIZE" -cdrom "$TMP_ISO_DIR/ubuntu-20.04-netboot-amd64-unattended.iso" -boot once=d "$DISK_FILE"'
 # remove tmp
 rm -r -f "$TMP_ISO_DIR"
-
 # done
 echo "Next steps: deploy image, login via root, adjust the authorized keys, set a root password (if you want to), deploy via ansible (if applicable), enjoy!"
